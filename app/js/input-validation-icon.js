b7bac1ae5b315e492db18ff56aff604c1a3c3dca
rinat_y <qwwert@mail.rui>
2017-01-19 12:02:34 +0300

    Исправлены инпуты в таблице. Добавлен эффект при заполнении инпута.

---
+++ app/js/input-validation-icon.js
@@ -0,0 +1,11 @@
+/**
+ * Created by rinat_y on 1/19/17.
+ */
+$(document).ready(function(){
+    function tog(v){return v?'addClass':'removeClass';}
+    $(document).on('input', '.clearable', function(){
+        $(this)[tog(this.value)]('x');
+    }).on('mousemove', '.x', function( e ){
+        $(this)[tog(this.offsetWidth-18 < e.clientX-this.getBoundingClientRect().left)]('onX');
+    });
+});
