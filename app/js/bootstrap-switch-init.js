8fd51748db5d021e10c4847915b65af46c4698df
Ilnur Shavaliev <shavalievoficial@gmail.com>
2017-01-27 17:37:09 +0300

    Начата страница diagnostick.html

--- app/js/bootstrap-switch-init.js
+++ app/js/bootstrap-switch-init.js
@@ -2,3 +2,9 @@
  * Created by rinat_y on 1/23/17.
  */
 $("[name='my-checkbox']").bootstrapSwitch();
+
+
+// my code
+$( ".bootstrap-switch" ).click(function() {
+    $( this ).toggleClass( "highlight" ); console.log('1111');
+});
