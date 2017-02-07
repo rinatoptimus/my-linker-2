87f4eff044b0eb54261b943be80bf696b384528c
Ilnur Shavaliev <shavalievoficial@gmail.com>
2017-02-07 10:18:34 +0300

    Страница vrachi - исправлены инпуты в табах, которые при маленьком разрешении не принимали фокус. Изменена высота сайдбара.

--- app/js/sidebar-rotate-icon.js
+++ app/js/sidebar-rotate-icon.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
     $('.collapsed').click(function(){
         $(this).find($('.fa')).toggleClass('down');
     })
+
+
+    $('.btn').click(function(){
+        $(this).find($('.fa-long-arrow-down')).toggleClass('show');
+        $(this).find($('.fa-long-arrow-up')).toggleClass('hide');
+    })
+
 });
