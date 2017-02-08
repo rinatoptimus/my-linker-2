5eb49292fa1569baecf7ab7bd3d6d0955b18f70e
Ilnur Shavaliev <shavalievoficial@gmail.com>
2017-02-08 16:33:18 +0300

    Код переключения чекбокса (Положительный-Отрицательный) вынесен в отдельный файл.

---
+++ app/js/checkbox-label-toggler.js
@@ -0,0 +1,7 @@
+$('.bootstrap-switch-success').click(function(){
+    $('.test').toggleClass('affirmative negative');
+});
+
+$('.bootstrap-switch-danger').click(function(){
+    $('.test').toggleClass('negative affirmative');
+});
