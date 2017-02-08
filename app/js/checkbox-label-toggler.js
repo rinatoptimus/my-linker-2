f5c35d5965e15d4b3e64b1e7499c5f778ecb39d4
Ilnur Shavaliev <shavalievoficial@gmail.com>
2017-02-08 16:34:39 +0300

    Изменено название класса чекбокса (Положительный-Отрицательный).

--- app/js/checkbox-label-toggler.js
+++ app/js/checkbox-label-toggler.js
@@ -1,7 +1,7 @@
 $('.bootstrap-switch-success').click(function(){
-    $('.test').toggleClass('affirmative negative');
+    $('.change-label').toggleClass('affirmative negative');
 });
 
 $('.bootstrap-switch-danger').click(function(){
-    $('.test').toggleClass('negative affirmative');
+    $('.change-label').toggleClass('negative affirmative');
 });
