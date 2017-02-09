33ab425403b84ffe7ec57bc0f877f35b04df2908
Ilnur Shavaliev <shavalievoficial@gmail.com>
2017-02-09 12:29:34 +0300

    Готова анимация зеленой точки для инпутов Имя и Телефон.

--- app/js/input-dot-animation.js
+++ app/js/input-dot-animation.js
@@ -1,16 +1,16 @@
 $(function() {
-    $("input").on("click", function(e) {
-        $(".green-dot").addClass("green-dot-hidden");
+    $(".js-testimonial-input").on("click", function(e) {
+        $(".green-dot-name").addClass("green-dot-hidden");
         e.stopPropagation()
     });
     $(document).on("click", function(e) {
         if ($(e.target).is("input") === false) {
-            $(".green-dot").removeClass("green-dot-hidden");
+            $(".green-dot-name").removeClass("green-dot-hidden");
         }
     });
 
 
-    $("input").on("click", function(e) {
+    $(".input-phone").on("click", function(e) {
         $(".green-dot-phone").addClass("green-dot-hidden");
         e.stopPropagation()
     });
